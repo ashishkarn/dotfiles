7dce082a6a6aa51063bcd4c2bbf3bf512e974563
ashish <ashish.karan96@gmail.com>
2026-02-24 00:15:46 +0530

    changes

---
+++ common/.config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,10 @@
+return {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = "NoNeckPain",
+    keys = {
+        { "<leader>cs", "<cmd>NoNeckPain<cr>", desc = "Center buffer" },
+    },
+    opts = {
+        width = 120,
+    },
+}
